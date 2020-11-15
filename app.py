841e737c7ad7253797a84c60c6052bcf1e36126b
Hemapratha <44408347+Hemapratha@users.noreply.github.com>
2020-11-16 00:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-
+//Get HTTP Request
 req=requests.get("https://www.nykaa.com/nykaa-naturals-face-wash/p/863854?categoryId=8380&pps=1&productId=863854&ptype=product&skuId=790570")
+//Get the content
 cont=req.content
 search=BeautifulSoup(cont,"html.parser")
+//Extract the price
 element=search.find("span",{"class" : "post-card__content-price-offer"})
 value=element.text.strip()
 original_price=float(value[1:])
+//Verify whether the price is reasonable
 if original_price<200:
     print("You can buy the product..The cost is {}".format(value))
 else:
